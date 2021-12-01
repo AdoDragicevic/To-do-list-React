7c904def6870fc9a6dc938f4dd66acfa58ef9e76
Ado Dragicevic <adodragicevic@gmail.com>
2021-12-01 04:57:47 +0100

    Finish App.js logic to find relevant items as props to TodoList.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import useToggle from "./hooks/useToggle";
 import todosReducer from "./reducers/todo.reducer";
 import { Todos, TodosReducer } from "./models/Todos";
+import { List } from "./models/Todos";
 
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
@@ -19,8 +20,9 @@ const App: React.FC = () => {
 
   const items = (() => {
     if (!todos.openListId) return todos.lists;
-    const items = todos.lists.find(list => list.id === todos.openListId)!.items;
-    return showCompleted ? items : items.filter(item => item.isCompleted === true);
+    const list = todos.lists.find(l => l.id === todos.openListId);
+    if (!list) throw new Error("todos.openListId does not mathc any list!");
+    return showCompleted ? list.items : list.items.filter(i => i.isCompleted);
   })();
 
   return (
